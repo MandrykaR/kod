1e73165750b369b026ba437773aa092a8d0fb709
Roman <mandrykaroman01@gmail.com>
2023-08-16 21:23:10 +0200

    tabl

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 5; i += 1) {
+    for (let j = 1; j <= 10; j += 1) {
+        const product = i * j;
+        console.log(`${i} * ${j} = ${product}`);
+    }
+    console.log("=>");
+}
